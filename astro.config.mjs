ee40afb443d173633ec682025b0c31cac456095f
lanzerDev <hdsnpej@gmail.com>
2024-09-10 15:49:51 -0600

    only works with astro 3.6.5? vercel?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import vercel from '@astrojs/vercel/serverless';
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
@@ -9,10 +8,5 @@ export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://portafolio-lanzerdev.dev/',
   output: 'server',
-  adapter: vercel(),
-  vite: {
-    ssr: {
-      noExternal: ['@fontsource-variable']
-    }
-  }
+  adapter: vercel()
 })
